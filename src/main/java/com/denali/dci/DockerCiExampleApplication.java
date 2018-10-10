1f92ab1f7664b3bb3a4bc4efb153acff24db1579
Ariv <arivbits@gmail.com>
2018-10-10 18:40:19 +0530

    first commit

---
+++ src/main/java/com/denali/dci/DockerCiExampleApplication.java
@@ -0,0 +1,12 @@
+package com.denali.dci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerCiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerCiExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/denali/dci/DockerCiExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.denali.dci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerCiExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
